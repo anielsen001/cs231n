fcd3a474b90a877f61823f937bb6a710f928eaf0
Aaron Nielsen <apn@apnielsen.com>
2018-01-08 12:56:28 -0500

    added example code from course website

---
+++ examples/classify.py
@@ -0,0 +1,4 @@
+Xtr, Ytr, Xte, Yte = load_CIFAR10('data/cifar10/') # a magic function we provide
+# flatten out all images to be one-dimensional
+Xtr_rows = Xtr.reshape(Xtr.shape[0], 32 * 32 * 3) # Xtr_rows becomes 50000 x 3072
+Xte_rows = Xte.reshape(Xte.shape[0], 32 * 32 * 3) # Xte_rows becomes 10000 x 3072
---
+++ examples/optim1.py
@@ -0,0 +1,39 @@
+# these are functions based on examples in the optimization-1  class
+# http://cs231n.github.io/optimization-1/
+
+def eval_numerical_gradient(f, x):
+  """ 
+  a naive implementation of numerical gradient of f at x 
+  - f should be a function that takes a single argument
+  - x is the point (numpy array) to evaluate the gradient at
+  """ 
+
+  fx = f(x) # evaluate function value at original point
+  grad = np.zeros(x.shape)
+  h = 0.00001
+
+  # iterate over all indexes in x
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+
+    # evaluate function at x+h
+    ix = it.multi_index
+    old_value = x[ix]
+    x[ix] = old_value + h # increment by h
+    fxh = f(x) # evalute f(x + h)
+    x[ix] = old_value # restore to previous value (very important!)
+
+    # compute the partial derivative
+    grad[ix] = (fxh - fx) / h # the slope
+    it.iternext() # step to next dimension
+
+  return grad
+
+
+# to use the generic code above we want a function that takes a single argument
+# (the weights in our case) so we close over X_train and Y_train
+def CIFAR10_loss_fun(W):
+  return L(X_train, Y_train, W)
+
+W = np.random.rand(10, 3073) * 0.001 # random weight vector
+df = eval_numerical_gradient(CIFAR10_loss_fun, W) # get the gradient
